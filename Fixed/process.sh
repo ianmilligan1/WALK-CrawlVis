1967fa907f1284872b76cdedba0b205ffa1bb8f8
Ian Milligan <ianmilligan1@gmail.com>
2016-12-04 16:21:55 +0000

    intermediate processing

--- Fixed/process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for filename in *.txt; do
-    filenameshort=${filename/-all.txt/}
-    echo $filenameshort
-    cp $filename /Users/ianmilligan1/dropbox/git/WALK-CrawlVis/crawl-sites/raw.txt
-    python2 /Users/ianmilligan1/dropbox/git/WALK-CrawlVis/crawl-sites/process.py > /Users/ianmilligan1/dropbox/git/WALK-CrawlVis/crawl-sites/$filenameshort.csv
-    sed "s/data.csv/$filenameshort.csv/g" /Users/ianmilligan1/dropbox/git/WALK-CrawlVis/crawl-sites/index.html > /Users/ianmilligan1/dropbox/git/WALK-CrawlVis/crawl-sites/$filenameshort.html
-done
